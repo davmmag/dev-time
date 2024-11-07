acb772a4629ebfdf1e3f08c069b4f384f8b10b89
davmmag <dreamdmm10@gmail.com>
2024-11-07 22:35:36 +0500

    feat: add files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,1 @@
-// const BURGER_BTN = document.querySelector('.burger-btn');
-// const MENU = document.querySelector('.menu');
-
-// const toggleMenu = () => {
-//   MENU.classList.toggle('menu--active');
-// }
-
-// BURGER_BTN.addEventListener('click', toggleMenu);
-
-console.log(1519 + 45849)
-48262
+import './styles/style.scss';
