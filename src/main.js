b9ac321ad32029f00450d80324dd3cad511f9d01
davmmag <dreamdmm10@gmail.com>
2024-11-12 22:20:21 +0500

    feat: begin realization slider

--- src/main.js
+++ src/main.js
@@ -21,3 +21,33 @@ const closeMenu = (e) => {
 
 BURGER.addEventListener('click', toggleMenu);
 NAV.addEventListener('click', closeMenu);
+
+
+// Slider
+const SLIDER__CONTROLS = document.querySelector('.slider__controls');
+const SLIDER__CONTAINER = document.querySelector('.slider__container-inner');
+console.log(SLIDER__CONTAINER.scrollWidth)
+
+const toNumber = (str) => {
+  if (typeof str === 'number') return str;
+  return Number(str.replace('px', ''));
+}
+const startSlider = (flag, elem ) => {
+  const STYLES_ELEM = window.getComputedStyle(elem);
+  let REAL_MEANING = STYLES_ELEM.getPropertyValue('--transform') || 0;
+  if (flag) {
+    SLIDER__CONTAINER.style.setProperty('--transform', `${toNumber(REAL_MEANING) + -135}px`);
+  }
+  if (flag === false) {
+    SLIDER__CONTAINER.style.setProperty('--transform', `${toNumber(REAL_MEANING) + 135}px`);
+  }
+};
+
+SLIDER__CONTROLS.addEventListener('click', (e) => {
+  if (e.target.classList.contains('slider__arrow--left')) {
+    startSlider(false, SLIDER__CONTAINER);
+  }
+  if (e.target.classList.contains('slider__arrow--right')) {
+    startSlider(true, SLIDER__CONTAINER);
+  }
+})
